ba5e0cf976b92f1c0279e16222de73161d72211e
markkong30 <markkong30@gmail.com>
2023-06-15 18:29:20 +0100

    add news route

--- main.py
+++ main.py
@@ -1,37 +1,16 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import requests
-from bs4 import BeautifulSoup
+
+from scrape import fetch_news
+
 
 app = Flask(__name__)
 CORS(app)  # Allow Cross-Origin Resource Sharing
 
 
-@app.route("/", methods=["GET"])
-def get_news():
-    # use requests library to get the page
-    response = requests.get("https://www.bbc.co.uk/news/world", verify=False)
-
-    # create BeautifulSoup object
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    # find all the articles
-    articles = soup.find_all("article")
-    newslist = []
-
-    # loop through each article to find the title, link, and image source
-    for article in articles:
-        try:
-            newsitem = article.find("h3", class_="lx-stream-post__header-title")
-            title = newsitem.text.strip()
-            link = newsitem.a["href"]
-            image = article.find("img", class_="qa-story-image")["src"]
-
-            newsarticle = {"title": title, "link": link, "image": image}
-            newslist.append(newsarticle)
-
-        except:
-            pass
+@app.route("/news", methods=["GET"])
+def render_news():
+    newslist = fetch_news()
 
     return jsonify({"data": newslist})
 
